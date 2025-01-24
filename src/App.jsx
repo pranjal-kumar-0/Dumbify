ddedd0ea5b35fad25f63029c268b25594593dad0
shaunakc11 <helloshaunak276@gmail.com>
2025-01-24 11:03:44 +0530

    dumb navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <nav>
-        <ul>
-          <li>Pricing</li>
+      <nav className='flex w-full items-center justify-between max-w-7xl mx-auto p-4'>
+        <h1 className='text-2xl font-bold'>Dumbify</h1>
+        <ul className='flex gap-4'>
           <li>Leaderboard</li>
+          <li>Pricing</li>
         </ul>
       </nav>
       <main></main>
